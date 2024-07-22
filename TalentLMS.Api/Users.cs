8b32dff7858aa427bacee79f3e990dbb38899f03
Mark Davis <markd@trifoia.com>
2024-07-22 15:53:28 -0700

    Refactor user signup method for URL encoding
    Changed the UserSignup method to accept data in URL-encoded format instead of StringDictionary for better compatibility.

--- TalentLMS.Api/Users.cs
+++ TalentLMS.Api/Users.cs
@@ -24,7 +24,7 @@ namespace TalentLMS.Api
         //Task<ApiResponse<Users.BasicUser>> UserSignupOld([Body] Users.NewUserQA data);
 
         [Post("/usersignup")]
-        Task<ApiResponse<Users.BasicUser>> UserSignup([Body] StringDictionary data);
+        Task<ApiResponse<Users.BasicUser>> UserSignup([Body(BodySerializationMethod.UrlEncoded)] Dictionary<string,object> data);
 
         [Get("/addusertobranch?user_id={userId}&branch_id={branchId}")]
         Task<ApiResponse<UserBranch>> AddUserToBranch(string userId, string branchId);
