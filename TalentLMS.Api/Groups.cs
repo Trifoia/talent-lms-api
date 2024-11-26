b79fadd8c950971531d155bea97a15cacf0c1b98
Bryce Fisher <bryce.e.fisher@gmail.com>
2024-11-26 11:44:43 -0800

    Enhanced Groups API functionality
    Added a new endpoint to create groups in the TalentLMS API. The BasicGroup record has been updated with nullable fields and type changes for better flexibility. A new record, CreateGroupRequest, was introduced to handle group creation requests.

--- TalentLMS.Api/Groups.cs
+++ TalentLMS.Api/Groups.cs
@@ -11,20 +11,31 @@ namespace TalentLMS.Api
 
         [Get("/groups?id={groupId}")]
         Task<ApiResponse<Groups.Group>> Group(string groupId);
+
+        [Post("/creategroup")]
+        Task<ApiResponse<Groups.BasicGroup>> CreateGroup([Body(BodySerializationMethod.UrlEncoded)]Dictionary<string, string> data);
     }
 
     namespace Groups
     {
         public record BasicGroup(
-            string Id,
+            int Id,
             string Name,
-            string Description,
-            string Key,
-            string Price,
+            string? Description,
+            string? Key,
+            string? Price,
             string OwnerId,
-            string? BelongToBranch,
+            string? BelongsToBranch,
             string? MaxRedemptions,
             string? RedemptionsSoFar);
+
+        public record CreateGroupRequest(
+            string Name,
+            string? Description = null,
+            string? Key = null,
+            string? Price = null,
+            string? CreatorId = null,
+            string? MaxRedemptions = null);
 
         public record Group(
             string Id,
@@ -49,3 +60,4 @@ namespace TalentLMS.Api
         }
     }
 }
+
